8ab59b8b29c827d22b8300bd756e19428edf2d20
almeidx <almeidx@pm.me>
2021-11-18 11:48:11 +0000

    finished ficha 6

--- fichas/ficha-6.c
+++ fichas/ficha-6.c
@@ -35,9 +35,12 @@ void readVector(int vector[], int size) {
 }
 
 void intercalateVectors(int v1[], int v2[], int v3[], int size) {
-    for (int i = 0; i <= size; i += 2) {
-        v3[i] = v1[i];
-        v3[i + 1] = v2[size - i - 1];
+    int j = 0;
+    for (int i = 0; i < size; i++) {
+        v3[j] = v1[i];
+        j++;
+        v3[j] = v2[size - i - 1];
+        j++;
     }
 }
 
@@ -47,6 +50,65 @@ void displayVector(int v[], int size) {
         printf("%d ", v[i]);
     }
     printf("}\n");
+}
+
+void displayVectorReverse(int v[], int size) {
+    printf("{ ");
+    for (int i = size - 1; i >= 0; i--) {
+        printf("%d ", v[i]);
+    }
+    printf("}\n");
+}
+
+void getMaxValue(int v[], int size, int *max) {
+    *max = v[0];
+    for (int i = 1; i < size; i++) {
+        if (v[i] > *max) {
+            *max = v[i];
+        }
+    }
+}
+
+void getMinValue(int v[], int size, int *min) {
+    *min = v[0];
+    for (int i = 1; i < size; i++) {
+        if (v[i] < *min) {
+            *min = v[i];
+        }
+    }
+}
+
+void getSum(int v[], int size, int *sum) {
+    *sum = 0;
+    for (int i = 0; i < size; i++) {
+        *sum += v[i];
+    }
+}
+
+void getMedian(int v[], int size, int *median) {
+    int sum = 0;
+    getSum(v, size, &sum);
+    *median = sum / size;
+}
+
+void getMedianBounds(int v[], int size, int *smaller, int *bigger) {
+    int median = 0;
+    getMedian(v, size, &median);
+    *smaller = 0;
+    *bigger = 0;
+    for (int i = 0; i < size; i++) {
+        if (v[i] > median) {
+            *bigger += 1;
+        } else if (v[i] < median) {
+            *smaller += 1;
+        }
+    }
+}
+
+void duplicateVector(int v1[], int v2[], int size) {
+    for (int i = 0; i < size; i++) {
+        v2[i] = v1[i];
+    }
 }
 
 void main() {
@@ -69,11 +131,60 @@ void main() {
     // printf("%s", str1);
 
     // Exercicio 4
-    int n, v1[VECTOR_SIZE], v2[VECTOR_SIZE], v3[DOUBLE_VECTOR_SIZE];
-    readInt(&n, "Introduza o tamanho dos vectores: ");
+    // int n, v1[VECTOR_SIZE], v2[VECTOR_SIZE], v3[DOUBLE_VECTOR_SIZE];
+    // readInt(&n, "Introduza o tamanho dos vectores: ");
+    // readVector(v1, n);
+    // readVector(v2, n);
+    // intercalateVectors(v1, v2, v3, n);
+    // displayVector(v3, n * 2);
+
+    // Exercicio 5
+    int v1[VECTOR_SIZE], n;
+    readInt(&n, "Introduza o tamanho do vector: ");
+
+    // a
     readVector(v1, n);
-    readVector(v2, n);
-    // broken
+
+    // b
+    displayVector(v1, n);
+
+    // c
+    displayVectorReverse(v1, n);
+
+    // d
+    int max;
+    getMaxValue(v1, n, &max);
+    printf("O valor máximo é %d\n", max);
+
+    // e
+    int min;
+    getMinValue(v1, n, &min);
+    printf("O valor mínimo é %d\n", min);
+
+    // f
+    int sum;
+    getSum(v1, n, &sum);
+    printf("A soma das componentes é %d\n", sum);
+
+    // g
+    int media;
+    getMedian(v1, n, &media);
+    printf("A média das componentes é %d\n", media);
+
+    // h
+    int bigger, smaller;
+    getMedianBounds(v1, n, &bigger, &smaller);
+    printf(
+        "Componentes maiores que a média: %d\nComponentes menores que a média: "
+        "%d\n",
+        bigger, smaller);
+
+    // i
+    int v2[VECTOR_SIZE];
+    duplicateVector(v1, v2, n);
+
+    // j
+    int v3[VECTOR_SIZE];
     intercalateVectors(v1, v2, v3, n);
     displayVector(v3, n * 2);
 }
